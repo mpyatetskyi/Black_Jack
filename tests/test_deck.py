a45298b74e1c9f017534131806540841156fc1bd
mpiatetskyi <m.pyatetskyi@gmail.com>
2021-01-12 14:30:38 +0200

    just my first and simple README FILE commited and pushed from the cmd

---
+++ tests/test_deck.py
@@ -0,0 +1,13 @@
+from src.black_jack.resources.classes_full import Deck
+
+def test_deck_init():
+    my_deck = Deck()
+    assert len(my_deck.deck) == 52
+
+def add_card_init():
+    my_deck = Deck()
+    my_deck.deal()
+    my_deck.deal()
+    assert len(my_deck.deck) == 50
+
+
