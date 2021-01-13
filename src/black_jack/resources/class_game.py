b36803987aa9b7ee0e9e393563964aa6e3bcb684
mpiatetskyi <m.pyatetskyi@gmail.com>
2021-01-13 17:00:57 +0200

    class_game.py pep8 style with no errors

--- src/black_jack/resources/class_game.py
+++ src/black_jack/resources/class_game.py
@@ -1,29 +1,36 @@
-from src.black_jack.resources.classes_full import *
+from src.black_jack.resources.classes_full import Deck
+from src.black_jack.resources.classes_full import Chips
+from src.black_jack.resources.classes_full import Hand
+from src.black_jack.resources.gameFunctions import player_wins
+from src.black_jack.resources.gameFunctions import dealer_busts
+
 
 class Game:
 
     def __init__(self):
         pass
 
-    def take_bet(self,chips):
+    def take_bet(self, chips):
         while True:
 
             try:
-                chips.bet = int(input("How many chips would you like to bet? "))
-            except:
+                chips.bet = int(input("How many chips "
+                                      "would you like to bet? "))
+            except ValueError:
                 print("Sorry, please insert an integer")
             else:
                 if chips.bet > chips.total:
-                    print(f"Sorry,you don`t have enough money. You have only {chips.total}")
+                    print(f"Sorry,you don`t have enough money."
+                          f" You have only {chips.total}")
                 else:
                     break
 
-    def hit(self,deck, hand):
+    def hit(self, deck, hand):
         single_card = deck.deal()
         hand.add_card(single_card)
         hand.adjust_for_ace()
 
-    def show_some(self,player, dealer):
+    def show_some(self, player, dealer):
         print("Dealer`s hand:")
         print("One card hidden")
         print(dealer.cards[1])
@@ -33,7 +40,7 @@ class Game:
             print(card)
         print(player.value)
 
-    def show_all(self,player, dealer):
+    def show_all(self, player, dealer):
         print("Dealer`s hand:")
         for card in dealer.cards:
             print(card)
@@ -44,25 +51,24 @@ class Game:
             print(card)
         print(player.value)
 
-    def player_busts(self,player, dealer, chips):
+    def player_busts(self, player, dealer, chips):
         print("Player busts")
         chips.loose_bet()
 
-    def player_wins(self,player, dealer, chips):
+    def player_wins(self, player, dealer, chips):
         print("Player wins!!!")
         chips.win_bet()
 
-    def dealer_busts(self,player, dealer, chips):
+    def dealer_busts(self, player, dealer, chips):
         print("Dealer busts, Player wins")
         chips.win_bet
 
-    def dealer_wins(self,player, dealer, chips):
+    def dealer_wins(self, player, dealer, chips):
         print("Dealer wins")
         chips.loose_bet()
 
-    def tie(self,player, dealer, chips):
+    def tie(self, player, dealer, chips):
         print("Tie. Nobody wins")
-
 
     def play(self):
 
@@ -76,55 +82,68 @@ class Game:
             self.player_hand = Hand()
             self.dealer_hand = Hand()
 
-            for _ in range (2):
+            for _ in range(2):
                 self.player_hand.add_card(self.deck.deal())
                 self.dealer_hand.add_card(self.deck.deal())
-
 
             player_chips = Chips()
 
             self.take_bet(player_chips)
 
-            self.show_some(self.player_hand,self.dealer_hand)
-
+            self.show_some(self.player_hand, self.dealer_hand)
 
             while True and self.player_hand.value < 21:
                 choice = input("Please choose h or s")
-                if choice[0].lower() not in ['s','h']:
+                if choice[0].lower() not in ['s', 'h']:
                     continue
 
                 elif choice[0] == 'h':
                     self.player_hand.add_card(self.deck.deal())
-                    self.show_some(self.player_hand, self.dealer_hand)
+                    self.show_some(self.player_hand,
+                                   self.dealer_hand)
                     continue
 
                 elif choice[0] == 's':
                     break
 
-            self.show_all(self.player_hand, self.dealer_hand)
+            self.show_all(self.player_hand,
+                          self.dealer_hand)
 
             if self.player_hand.value == 21:
-                self.player_wins(self.player_hand,self.dealer_hand,player_chips)
+                self.player_wins(self.player_hand,
+                                 self.dealer_hand,
+                                 player_chips)
 
             elif self.player_hand.value > 21:
-                self.player_busts(self.player_hand, self.dealer_hand, player_chips)
+                self.player_busts(self.player_hand,
+                                  self.dealer_hand,
+                                  player_chips)
             elif self.player_hand.value < 21:
-                while self.dealer_hand.value <=17:
+                while self.dealer_hand.value <= 17:
                     self.dealer_hand.add_card(self.deck.deal())
 
-                self.show_all(self.player_hand,self.dealer_hand)
+                self.show_all(self.player_hand,
+                              self.dealer_hand)
 
                 if self.dealer_hand.value > 21:
-                    dealer_busts(self.player_hand,self.dealer_hand,player_chips)
+                    dealer_busts(self.player_hand,
+                                 self.dealer_hand,
+                                 player_chips)
 
                 elif self.dealer_hand.value > self.player_hand.value:
-                    self.dealer_wins(self.player_hand, self.dealer_hand, player_chips)
+                    self.dealer_wins(self.player_hand,
+                                     self.dealer_hand,
+                                     player_chips)
 
                 elif self.dealer_hand.value < self.player_hand.value:
-                    player_wins(self.player_hand,self.dealer_hand,player_chips)
+                    player_wins(self.player_hand,
+                                self.dealer_hand,
+                                player_chips)
 
                 else:
-                    self.tie(self.player_hand,self.dealer_hand,player_chips)
+                    self.tie(self.player_hand,
+                             self.dealer_hand,
+                             player_chips)
 
             print(f'\n Players total money {player_chips.total}')
 
@@ -135,18 +154,9 @@ class Game:
                 print("Thanks for playing!")
                 playing = False
             else:
-                game_over = False
-
+                playing = True
 
 
 if __name__ == '__main__':
     game = Game()
     game.play()
-
-
-
-
-
-
-
-
