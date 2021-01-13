687babd8758aa4c2cf39a4b61c96e04fcc8ad6b8
mpiatetskyi <m.pyatetskyi@gmail.com>
2021-01-13 11:45:12 +0200

    test_deck.py tests update

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -1,13 +1,14 @@
 from src.black_jack.resources.classes_full import Deck
 
+
 def test_deck_init():
     my_deck = Deck()
     assert len(my_deck.deck) == 52
+
 
 def add_card_init():
     my_deck = Deck()
     my_deck.deal()
     my_deck.deal()
-    assert len(my_deck.deck) == 50
-
-
+    my_deck.deal()
+    assert len(my_deck.deck) == 49
