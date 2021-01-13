f12a44fd9cbc4ef069fa16522829ac434f2c0f64
mpiatetskyi <m.pyatetskyi@gmail.com>
2021-01-13 11:59:41 +0200

    test pytest in circle ci

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -6,9 +6,4 @@ def test_deck_init():
     assert len(my_deck.deck) == 52
 
 
-def add_card_init():
-    my_deck = Deck()
-    my_deck.deal()
-    my_deck.deal()
-    my_deck.deal()
-    assert len(my_deck.deck) == 52
+
