86fcbcc9b2bcf452d03a8a23229b7dbd1dff4f73
mpiatetskyi <m.pyatetskyi@gmail.com>
2021-01-29 14:08:27 +0200

    test_chips.py with no mistakes

--- tests/test_chips.py
+++ tests/test_chips.py
@@ -16,11 +16,10 @@ def test_bet_default():
     assert my_chips.bet == 0
 
 
-
 def test_take_a_bet():
     my_chips = Chips()
     my_chips.take_a_bet(50)
-    assert my_chips.bet == 51
+    assert my_chips.bet == 50
 
 
 def test_win_bet():
